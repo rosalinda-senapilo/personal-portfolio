450079ec76660367c5929993b68a5067ac6cfbf9
unknown <rosalinda@fullstack.ph>
2024-04-23 13:33:40 +0800

    initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import { Socials } from "@/constants";
+import Image from "next/image";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="fixed top-0 z-[40] w-full h-[100px] bg-transparent flex justify-between items-center px-10 md:px-20 py-16">
+      <div className="flex flex-row gap-3 items-center">
+        <div className="relative">
+          <Image
+            src="/site.png"
+            alt="logo"
+            width={150}
+            height={150}
+            className="w-full h-full object-contain rounded-full"
+          />
+        </div>
+      </div>
+      <div className="flex flex-row gap-5 mb-2">
+        {Socials.map((social) => (
+          <Image
+            key={social.name}
+            src={social.src}
+            alt={social.name}
+            width={28}
+            height={28}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
